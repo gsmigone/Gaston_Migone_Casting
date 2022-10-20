fb92b92f6f9fa2fa6f59297a0e156fe22f51377f
gaston migone <gsmigone@gmail.com>
2022-10-20 20:15:27 -0300

    Agregar archivos de proyecto.

---
+++ Gaston_Migone_Casting/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Gaston_Migone_Casting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImp = new System.Windows.Forms.Button();
+            this.btnExp = new System.Windows.Forms.Button();
+            this.btnMet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImp
+            // 
+            this.btnImp.Location = new System.Drawing.Point(151, 62);
+            this.btnImp.Name = "btnImp";
+            this.btnImp.Size = new System.Drawing.Size(158, 23);
+            this.btnImp.TabIndex = 0;
+            this.btnImp.Text = "Ejemplo Casting Implícito";
+            this.btnImp.UseVisualStyleBackColor = true;
+            this.btnImp.Click += new System.EventHandler(this.btnImp_Click);
+            // 
+            // btnExp
+            // 
+            this.btnExp.Location = new System.Drawing.Point(151, 103);
+            this.btnExp.Name = "btnExp";
+            this.btnExp.Size = new System.Drawing.Size(158, 23);
+            this.btnExp.TabIndex = 1;
+            this.btnExp.Text = "Ejemplo Casting Explícito";
+            this.btnExp.UseVisualStyleBackColor = true;
+            this.btnExp.Click += new System.EventHandler(this.btnExp_Click);
+            // 
+            // btnMet
+            // 
+            this.btnMet.Location = new System.Drawing.Point(151, 146);
+            this.btnMet.Name = "btnMet";
+            this.btnMet.Size = new System.Drawing.Size(158, 23);
+            this.btnMet.TabIndex = 2;
+            this.btnMet.Text = "Ejemplo Métodos de Casteo";
+            this.btnMet.UseVisualStyleBackColor = true;
+            this.btnMet.Click += new System.EventHandler(this.btnMet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 265);
+            this.Controls.Add(this.btnMet);
+            this.Controls.Add(this.btnExp);
+            this.Controls.Add(this.btnImp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImp;
+        private System.Windows.Forms.Button btnExp;
+        private System.Windows.Forms.Button btnMet;
+    }
+}
+
---
+++ Gaston_Migone_Casting/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gaston_Migone_Casting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImp_Click(object sender, EventArgs e)
+        {
+            int variableEntera = 20;
+            double variableDouble = variableEntera;       // Casteo automático
+
+            Imprimir("Se define la siguiente variable: int variableEntera = 20");
+            Imprimir(variableEntera +  " es de tipo " + variableEntera.GetType());
+            Imprimir("Se realiza un casteo automático: double variableDouble = variableEntera");
+            Imprimir(variableDouble +  " es de tipo " + variableDouble.GetType());
+             
+        }
+
+        private void btnExp_Click(object sender, EventArgs e)
+        {
+            double variableDouble = 20.10;
+            int variableEntera = (int)variableDouble;    // Casteo manual
+
+            Imprimir("Se define la siguiente variable: double variableDouble = 20.10");
+            Imprimir(variableDouble +  " es de tipo " + variableDouble.GetType());
+            Imprimir("Se realiza un casteo manual:  int variableEntera = (int)variableDouble  (Al pasar de Double a int, deberá redondear el valor)");
+            Imprimir(variableEntera +  " es de tipo " + variableEntera.GetType());      
+        }
+
+
+        private void btnMet_Click(object sender, EventArgs e)
+        {
+            int variableEntera = 14;
+            double variableDouble = 6.66;
+            bool variableBooleana = false;
+
+            Imprimir("Se define la siguiente variable: int variableEntera = 14");
+            Imprimir("Se utiliza Convert.ToString(variableEntera) para convertirla en String");
+            Imprimir(Convert.ToString(variableEntera));    // Convierte entero en String
+            Imprimir("Se utiliza Convert.ToDouble(variableEntera) para convertirla en Double");
+            Imprimir(Convert.ToDouble(variableEntera));    // Convierte entero en Double
+            Imprimir("Se define la siguiente variable: double variableDouble = 6.66;");
+            Imprimir("Se utiliza Convert.ToInt32(variableDouble) para convertirla en Entero");
+            Imprimir(Convert.ToInt32(variableDouble));  // Convierte Double en int
+            Imprimir("Se define la siguiente variable: bool variableBooleana = false");
+            Imprimir("Se utiliza Convert.ToString(variableBooleana) para convertirla en String");
+            Imprimir(Convert.ToString(variableBooleana));   // Convierte Booleano en String
+        }
+
+
+        public void Imprimir(string texto)
+        {
+            MessageBox.Show(texto);
+        }
+
+        public void Imprimir(double texto)
+        {
+            MessageBox.Show(texto.ToString());
+        }
+    }
+}
